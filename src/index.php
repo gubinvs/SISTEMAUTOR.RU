b0851eac2fd6e5dcce65b4ca5fe8500bf9a4d0ba
gubinvs@gmail.com <gubinvs@gmail.com>
2025-05-13 14:21:06 +0300

    a

--- src/index.php
+++ src/index.php
@@ -6,25 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="https://encomponent.ru/favicon.svg" type="image/svg+xml">
-    <link rel="stylesheet" href="css/encomp-nku-project-style.css" media="all">
-    <meta name="description" content="Производство автоматизированных систем для реализации проектов в реальных секторах экономики: 
-                                        промышленных производствах, энергетике, машиностроении и других отраслях обеспечивающих материальное производство.">
-    <title>Производство автоматизированных систем управления - компоненты энергии</title>
+    <link rel="stylesheet" href="css/sistemautor-style.css" media="all">
+    <meta name="description" content="Системный менеджмент —это про управляемый рост, в котором бизнес становится независимым от людей и обретает внутреннюю устойчивость.">
+    <title>Системный менеджмент как инструмент для обеспечения роста компаний</title>
 </head>
 
 <body>
     <!--Подключение header-->
-    <?php include "php/modules/header.php" ?>
+    <?php ?>
     <!--Основная секция страницы-->
     <main>
-        <?php include "php/modules/main_index_page_section.php" ?>
-        <?php include "php/modules/index_solve-problems_section.php" ?>
-        <?php include "php/modules/index_case_section.php" ?>
+        <?php include "php/modules/main-section.php" ?>
     </main>
-    <!--Подключение header-->
-    <?php include "php/modules/footer.php" ?>
+    <!--Подключение footer-->
+ 
     <!--Подключение скриптов JS-->
     <script src="js/app.js" type="module"></script>
 </body>
-
 </html>
---
+++ src/php/modules/main-section.php
@@ -0,0 +1,6 @@
+<section class="main-section">
+    <div class="container main-section__container">
+        <h1 class="h1-min">Систематизация бизнес-процессов через разработку регламентов и инструкций</h1>
+
+    </div>
+</section>
