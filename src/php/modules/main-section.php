294e09f35213dfcfd04dea432596e24cce6098b1
gubinvs@gmail.com <gubinvs@gmail.com>
2025-05-13 15:26:45 +0300

    a

--- src/php/modules/main-section.php
+++ src/php/modules/main-section.php
@@ -1,6 +1,20 @@
 <section class="main-section">
     <div class="container main-section__container">
         <h1 class="h1-min">Систематизация бизнес-процессов через разработку регламентов и инструкций</h1>
-
+        <img src="../../img/logo_1920.svg" class="main-section__logo-img" alt="Логотип компании в виде букв К и Э с гаячным ключем внутри, надпись внизу компоненты энергии">
+        <img src="../../img/main-section-img_1920.png" alt="Главная картинка страницы, где рука на синем фоне нажимает на блок в структуре компании на блок генерального директора, тем самый включая его в работу и запуская систему">
+        <img src="../../img/soply_1920.png" class="main-section__abstract-img" alt="Абстрактная материя">
+        <div class="main-section__title-block">
+            <div class="main-section__slogan-block">
+                <div class="slogan">Настраиваем управление — как бизнес-инструмент</div>
+                <div class="cirkul-block">
+                    <img src="../../img/ellipse_1.png" alt="Синяя точка" class="cirkul_1">
+                    <img src="../../img/ellipse_2.png" alt="Красная точка" class="cirkul_2">
+                    <img src="../../img/ellipse_3.png" alt="Желтая точка" class="cirkul_3">
+                </div>
+            </div>
+            <div class="title-block__title">СИСТЕМНЫЙ МЕНЕДЖМЕНТ</div>
+            <div class="title-block__discr">не просто управление - это архитектура роста</div>
+        </div>
     </div>
 </section>
