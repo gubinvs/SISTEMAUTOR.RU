1a4628a4834a49ef93407124e1b19adfc826bc68
gubinvs@gmail.com <gubinvs@gmail.com>
2025-05-15 11:53:11 +0300

    a

--- src/php/modules/main-section.php
+++ src/php/modules/main-section.php
@@ -1,8 +1,14 @@
 <section class="main-section">
     <div class="container main-section__container">
         <h1 class="h1-min">Систематизация бизнес-процессов через разработку регламентов и инструкций</h1>
-        <img src="../../img/main-section-img_1920.png" alt="Главная картинка страницы, где рука на синем фоне нажимает на блок в структуре компании на блок генерального директора, тем самый включая его в работу и запуская систему">
-        <img src="../../img/soply_1920.png" class="main-section__abstract-img" alt="Абстрактная материя">
+        <picture>
+            <source srcset="../../img/main-section-img_1920.png" media="(min-width: 1281px)">
+            <source srcset="../../img/main-section-img_1920.png" media="(max-width: 1280px) and (min-width: 1025px)">
+            <source srcset="../../img/main-section-img_1920.png" media="(max-width: 1024px) and (min-width: 961px)">
+            <source srcset="../../img/main-section-img_1920.png" media="(max-width: 960px)">
+            <img src="../../img/main-section-img_1920.png" alt="Главная картинка страницы, где рука на синем фоне нажимает на блок в структуре компании на блок генерального директора, тем самый включая его в работу и запуская систему">
+        </picture>
+       <img src="../../img/soply_1920.png" class="main-section__abstract-img" alt="Абстрактная материя">
         <div class="main-section__title-block">
             <div class="main-section__slogan-block">
                 <div class="slogan">Настраиваю управление — как бизнес-инструмент</div>
@@ -22,6 +28,7 @@
                 <img src="../../img/telegram-icon_1920.svg" class="social-icon" alt="Иконка телеграмма">
                 <img src="../../img/mail-icon_1920.svg" class="social-icon" alt="Иконка email">
                 <img src="../../img/vk-icon_1920.svg" class="social-icon" alt="Иконка вовконтакте">
+            </div>
         </div>
     </div>
 </section>
