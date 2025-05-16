72ea992bd3f6917e2e64a8dd1594731c8ac37126
gubinvs@gmail.com <gubinvs@gmail.com>
2025-05-16 11:18:47 +0300

    a

--- src/php/modules/main-section.php
+++ src/php/modules/main-section.php
@@ -3,8 +3,9 @@
         <h1 class="h1-min">Систематизация бизнес-процессов через разработку регламентов и инструкций</h1>
         <picture>
             <source srcset="../../img/main-section-img_1280.png" media="(max-width: 1280px) and (min-width: 1025px)">
-            <source srcset="../../img/main-section-img_1024.png" media="(max-width: 1024px) and (min-width: 800px)">
-            <source srcset="../../img/main-section-img_800.png" media="(max-width: 800px)">
+            <source srcset="../../img/main-section-img_1024.png" media="(max-width: 1024px) and (min-width: 801px)">
+            <source srcset="../../img/main-section-img_800.png" media="(max-width: 800px) and (min-width: 641px)">
+            <source srcset="../../img/main-section-img_640.png" media="(max-width: 640px)">
             <img src="../../img/main-section-img_1920.png" alt="Главная картинка страницы, где рука на синем фоне нажимает на блок в структуре компании на блок генерального директора, тем самый включая его в работу и запуская систему">
         </picture>
         <picture>
@@ -22,16 +23,17 @@
                 </div>
             </div>
             <div class="title-block__title">СИСТЕМНЫЙ МЕНЕДЖМЕНТ</div>
-            <div class="title-block__discr">не просто управление - это архитектура роста</div>
-        </div>
-        <div class="social-icon-bock">
-            <div class="social-icon-block__title">Способы для связи:</div>
-            <div class="social-icon-block__icon">
-                <img src="../../img/phone-icon_1920.svg" class="social-icon" alt="Иконка телефона">
-                <img src="../../img/telegram-icon_1920.svg" class="social-icon" alt="Иконка телеграмма">
-                <img src="../../img/mail-icon_1920.svg" class="social-icon" alt="Иконка email">
-                <img src="../../img/vk-icon_1920.svg" class="social-icon" alt="Иконка вовконтакте">
+            <div class="title-block__discr">не просто управление - это архитектура роста</div>        
+            <div class="social-icon-bock">
+                <div class="social-icon-block__title">Способы для связи:</div>
+                <div class="social-icon-block__icon">
+                    <img src="../../img/phone-icon_1920.svg" class="social-icon" alt="Иконка телефона">
+                    <img src="../../img/telegram-icon_1920.svg" class="social-icon" alt="Иконка телеграмма">
+                    <img src="../../img/mail-icon_1920.svg" class="social-icon" alt="Иконка email">
+                    <img src="../../img/vk-icon_1920.svg" class="social-icon" alt="Иконка вовконтакте">
+                </div>
             </div>
         </div>
+
     </div>
 </section>
